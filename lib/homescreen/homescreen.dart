8a79fd2c4dd613b19283f37c4f5919bdafe2926b
sanchaman_limbu <sanchamanmabo1@gmail.com>
2024-05-25 09:18:34 +0545

    initial commit

--- lib/homescreen/homescreen.dart
+++ lib/homescreen/homescreen.dart
@@ -1,67 +1,51 @@
-import 'package:favoriteitemshow/favoriteitem/favoriteItem.dart';
-import 'package:favoriteitemshow/provider/itemprovider.dart';
+
+import 'package:favoriteitemshow/homescreen/favoriteItem.dart';
+import 'package:favoriteitemshow/homescreen/homepage.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../constant/tile.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int selectedIndex = 0;
+
+  void onTabTapped(int index){
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  final List<Widget> pages = [
+    const HomePage(),
+    const FavoriteItem(),
+  ];
+
+
+  @override
   Widget build(BuildContext context) {
-    final favoriteAddedItem = Provider.of<ItemProvider>(context);
 
     return Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: AppBar(
-        title: const Text('Favorite App'),
-        centerTitle: true,
-        backgroundColor: Colors.blueGrey,
-        actions: [
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const FavoriteItem()),
-              );
-            },
-            child: const Icon(Icons.favorite, color: Colors.red),
-          ),
-          const SizedBox(width: 20),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: objects.length,
-              itemBuilder: (context, index) {
-                final currentTile = objects[index];
-                return ListTile(
-                  onTap: () {
-                    if (favoriteAddedItem.selectedItem.contains(index)) {
-                      favoriteAddedItem.removeItem(index);
-                    } else {
-                      favoriteAddedItem.addItem(index);
-                    }
-                  },
-                  leading: CircleAvatar(
-                    child: Image.asset(currentTile.image,),
-                  ),
-                  title: Text(currentTile.title,style: TextStyle(fontWeight: FontWeight.bold),),
-                  subtitle: Text(currentTile.subTitle),
-                  trailing: Icon(favoriteAddedItem.selectedItem.contains(index)
-                      ? Icons.favorite
-                      : Icons.favorite_border_outlined,
-                  color: favoriteAddedItem.selectedItem.contains(index) ? Colors.red : null,),
-                );
+      backgroundColor: Colors.grey.shade300,
+      body: pages[selectedIndex],
 
-              },
-            ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.orange.shade800,
+        backgroundColor: Colors.grey.shade400,
+        currentIndex: selectedIndex,
+        onTap: onTabTapped,
+        items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite),label: 'Favorite'),
 
-          ),
-        ],
+
+      ],
       ),
     );
   }
